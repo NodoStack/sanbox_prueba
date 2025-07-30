632d3c9de64284cffdea9134929bafdcc51869f6
carla <arevalocarlaelizabeth@gmail.com>
2025-07-30 16:45:21 -0300

    modificar manage.py para generar tablas

--- backend/ricco_app/apps.py
+++ backend/ricco_app/apps.py
@@ -9,13 +9,24 @@ class RiccoAppConfig(AppConfig):
     def ready(self):
         from django.contrib.auth import get_user_model
         from django.db.utils import OperationalError, ProgrammingError
+        from django.core.management import call_command
+
         try:
+            # APLICAR MIGRACIONES ANTES DE CREAR EL SUPERUSER
+            call_command('migrate')
+            print("✅ Migraciones aplicadas")
+
             User = get_user_model()
             if not User.objects.filter(username='admin').exists():
+                print('🛠️ Creando superusuario...')
                 User.objects.create_superuser(
                     username='admin',
                     email='admin@example.com',
                     password='adminpassword123'
                 )
+            else:
+                print('✅ Superusuario ya existe.')
         except (OperationalError, ProgrammingError) as e:
-            print("🚨 No se pudo crear el superusuario todavía:", e)
+            print("🚨 Error de base de datos al iniciar:", e)
+        except Exception as e:
+            print("❌ Otro error:", e)
