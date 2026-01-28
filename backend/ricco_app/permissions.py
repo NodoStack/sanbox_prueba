ae653cbd761bf7dfe4a7f5b89ff57274e3db45cc
MarianCos <cosmarian@gmail.com>
2026-01-28 13:31:03 -0300

    Estructura final limpia para deploy

--- backend/ricco_app/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class EsAdministradorPorRol(BasePermission):
-    def has_permission(self, request, view):
-        user = request.user
-        if not user or not user.is_authenticated:
-            return False
-        return user.rol and user.rol.nombre_rol.lower() == 'administrador'
